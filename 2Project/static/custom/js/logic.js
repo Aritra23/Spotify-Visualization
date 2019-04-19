2ff688ea31691542bb467bb32b2c1f11d1ffcf59
Tracer397 <matt.hum7@gmail.com>
2019-04-19 11:07:12 -0700

    updated table

--- 2Project/static/custom/js/logic.js
+++ 2Project/static/custom/js/logic.js
@@ -25,5 +25,18 @@ d3.csv('/get_data').then(function (data) {
     backgroundColor: '#000000', // #383f47
     markers: mapMarkers
   });
+
+  var tbody = d3.select("tbody");
+  console.log(data);
+  data.forEach(function(info) {
+    console.log(info);
+    var row = tbody.append("tr");
+    Object.entries(info).forEach(function([index,value]) {
+      console.log(value);
+      var cell = tbody.append("td");
+      cell.text(value);
+    });
+  });
+    
 });
 
