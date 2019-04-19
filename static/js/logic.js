df255dd38cb950fa7682ee2557df04071fa8a6a4
Aritra Kumar Lahiri <lahiri.aritra@gmail.com>
2019-04-18 18:41:13 -0700

    Add json files

--- static/js/logic.js
+++ static/js/logic.js
@@ -12,19 +12,63 @@ var myMap = L.map("map", {
     accessToken: API_KEY
   }).addTo(myMap);
   
-var file = "../spotify_data_lat_long_separated.csv";
+var link = "spotify_data_lat_long_separated.json";
 
-d3.csv(file, function(response, layer) {
+var apiLink = "https://developer.spotify.com/console/get-artist-top-tracks/?id=0TnOYISbd1XYRBk9myaseg&country=US";
+var geoJson;
+d3.json(apiLink, function(data) {
+  // Creating a geoJSON layer with the retrieved data
+  // geoJson = L.geoJson(data, {
+  //   // Style for each feature (in this case a neighborhood)
+    
+  //   // Called on each feature
+     //onEachFeature: function(feature, layer) {
+  //     // Setting various mouse events to change style when different events occur
+  //     layer.on({
+  //       // On mouse over, make the feature (neighborhood) more visible
+  //       mouseover: function(event) {
+  //         layer = event.target;
+  //         layer.setStyle({
+  //           fillOpacity: 0.9
+  //         });
+  //       },
+  //       // Set the features style back to the way it was
+  //       mouseout: function(event) {
+  //         geoJson.resetStyle(event.target);
+  //       },
+  //       // When a feature (neighborhood) is clicked, fit that feature to the screen
+  //       click: function(event) {
+  //         map.fitBounds(event.target.getBounds());
+  //       }
+  //     });
+  //     // Giving each feature a pop-up with information about that specific feature
+  //     layer.bindPopup("<h1>" + feature.Artist + "</h1> <hr> <h2>" + feature.region + "</h2>");
+  //   }
+  var markers = L.markerClusterGroup();
 
-  console.log(response);
-  //var markers = L.markerClusterGroup();
-  // 
-  for (var i = 0; i < response.length; i++) {
-    var location = response[i].city_coord;
-    console.log(location);
+  // Loop through data
+  for (var i = 0; i < data.length; i++) {
+
+    // Set the data location property to a variable
+    var location = data[i].location;
+
+    // Check for location property
     if (location) {
-      layer.bindPopup("<h1>" + response[i].city_coord + "</h1><hr> <h2>"+response[i].Artist+"</h2>");
+
+      // Add a new marker to the cluster group and bind a pop-up
+      markers.addLayer(L.marker([location.coordinates[1], location.coordinates[0]])
+        .bindPopup(response[i].descriptor));
     }
+
   }
-  // myMap.addLayer(markers);
-});
+
+  // Add our marker cluster layer to the map
+  myMap.addLayer(markers);
+
+
+
+
+
+     });
+    
+  
